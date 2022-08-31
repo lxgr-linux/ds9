c2ef49e22b9cc845478ba653cc83fb734264eb20
lxgr-linux <lxgr@protonmail.com>
2022-08-31 13:32:16 +0200

    Added tries counter

--- Main.gd
+++ Main.gd
@@ -4,18 +4,24 @@ extends Spatial
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
+var count = 0
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	_set_count_label()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	$Control2/Debug.text = "Gyroscope: " + str(Input.get_gyroscope())
 	if $Kugel.translation.y < -20:
+		self.count += 1
 		_reset()
+
+func _set_count_label():
+	$Control2/Counter.text = "Tries: " + str(self.count)
+	
 
 func _on_Ziel_body_entered(body):
 	print("win: " + str(body))
@@ -23,6 +29,7 @@ func _on_Ziel_body_entered(body):
 	get_tree().paused = true
 
 func _reset():
+	_set_count_label()
 	$Platte.rotation = $Platte.startrotation
 	$Kugel.translation = $Kugel.startposition
 	$Kugel.linear_velocity = Vector3.ZERO
@@ -31,4 +38,5 @@ func _reset():
 func _on_Win_pressed():
 	$Control/Win.visible = false
 	get_tree().paused = false
+	self.count = 0
 	_reset()
